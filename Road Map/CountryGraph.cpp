07e8f8d3c25f2fcc23170d591f314aaf0ca26742
tamergy <abdelrahmantamer0111@gmail.com>
2024-04-29 18:31:30 +0300

    Initial commit

---
+++ Road Map/CountryGraph.cpp
@@ -0,0 +1,196 @@
+#include "CountryGraph.h"
+#include<iostream>
+#include <sstream>
+#include <fstream>
+#include<vector>
+using namespace std;
+
+void CountryGraph::AddCity(string newcity) {
+
+    cities[newcity];//o(1)
+
+}
+
+void CountryGraph::AddEdge(string city_1, string city_2, int cost) {
+    if (!FindEdge(city_1, city_2))
+    {
+        //cairo alex 10
+        edge newedge1;
+        newedge1.destination_city = city_1;//cairo
+        newedge1.cost = cost;//10
+        cities[city_2].push_back(newedge1);//o(1)
+        //alex.list(cairo,10)
+        edge newedge2;
+        newedge2.destination_city = city_2;//alex
+        newedge2.cost = cost;//10
+        cities[city_1].push_back(newedge2);//o(1)
+        //cairo.list(alex,10)
+    }
+
+}
+
+bool CountryGraph::FindCity(string city)
+{
+    return cities.find(city) != cities.end();//o(1) 
+
+}
+
+bool CountryGraph::FindEdge(string city_1, string city_2)
+{
+    for (auto& edges : cities[city_1])
+    {
+        if (edges.destination_city == city_2)
+        {
+            return true;
+        }
+    }
+    return false;
+
+}
+
+void CountryGraph::DeleteCity(string deletedcity)
+{
+    cities.erase(deletedcity);
+    for (auto& city : cities)//O(V*E)
+    {
+        auto& edges = city.second;
+        edges.erase(remove_if(edges.begin(), edges.end(), [deletedcity](const edge& e) {
+            return e.destination_city == deletedcity;
+            }), edges.end());
+    }
+}
+
+void CountryGraph::DeleteEdge(string city_1, string city_2) //O(E)
+{
+
+    if (FindCity(city_1) and FindCity(city_2))
+    {
+        cities[city_1].erase(std::remove_if(cities[city_1].begin(), cities[city_1].end(), [city_2](const edge& e) {
+            return e.destination_city == city_2;
+            }), cities[city_1].end());
+
+        cities[city_2].erase(std::remove_if(cities[city_2].begin(), cities[city_2].end(), [city_1](const edge& e) {
+            return e.destination_city == city_1;
+            }), cities[city_2].end());
+    }
+}
+
+void CountryGraph::Delete_AllGraph()
+{
+    for (auto& city : cities)
+    {
+        DeleteCity(city.first);
+    }
+
+}
+
+void CountryGraph::DisplayGraph()
+{
+    for (auto& city : cities)//o(n^2)
+    {
+        cout << "(" << city.first << ")" << "------>";
+        for (auto& Edges : city.second)
+            cout << "(" << Edges.destination_city << "," << Edges.cost << ")" << "------>";
+        cout << endl;
+    }
+}
+
+void CountryGraph::DisplayCities()
+{
+    for (auto& city : cities)
+    {
+        cout << city.first << endl;
+    }
+}
+
+void CountryGraph::DisplayEdges()
+{
+    for (auto& city : cities)
+        for (auto& edge : city.second)
+        {
+            cout << "(" << city.first << "," << edge.destination_city << "," << edge.cost << ")" << endl;
+
+        }
+}
+
+int CountryGraph::Write_Cities_InFiles()
+{
+    string filename = "cities.txt";
+    ofstream clearFile(filename);
+    ofstream outfile(filename);
+    if (!outfile.is_open()) {
+        cerr << "Error opening file: " << filename << endl;
+        return 1;
+    }
+    for (auto& city : cities)
+    {
+        outfile << city.first << endl;
+    }
+    outfile.close();
+    cout << " City saved to file: " << filename << endl;
+}
+
+int CountryGraph::Write_Edges_InFiles()
+{
+    string filename = "edges.txt";
+    ofstream clearFile(filename);
+    ofstream outfile(filename);
+    if (!outfile.is_open()) {
+        cerr << "Error opening file: " << filename << endl;
+        return 1;
+    }
+    for (auto& city : cities)
+        for (auto& edge : city.second)
+        {
+            outfile << city.first << "," << edge.destination_city << "," << edge.cost << endl;
+
+        }
+    outfile.close();
+    cout << " Edges saved to file: " << filename << endl;
+}
+
+int CountryGraph::Read_Cities_FromFiles()
+{
+    string filename = "cities.txt";
+    vector<string> cities;
+    ifstream infile(filename);
+    if (!infile.is_open()) {
+        cerr << "Error opening file: " << filename << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(infile, line))
+    {
+        AddCity(line);
+    }
+    infile.close();
+}
+
+int CountryGraph::Read_Edges_FromFiles()
+{
+    string filename = "edges.txt";
+
+
+    ifstream infile(filename);
+    if (!infile.is_open()) {
+        cerr << "Error opening file: " << filename << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(infile, line)) {
+        vector<string> data_tokens;
+        string token;
+        istringstream tokenStream(line);
+        while (getline(tokenStream, token, ',')) {
+            data_tokens.push_back(token);
+        }
+
+        if (data_tokens.size() == 3)
+        {
+            AddEdge(data_tokens[0], data_tokens[1], stoi(data_tokens[2]));
+        }
+    }
+    infile.close();
+}
---
+++ Road Map/CountryGraph.h
@@ -0,0 +1,28 @@
+#include<unordered_map>
+#include<list>
+#include<string>
+using namespace std;
+struct edge
+{
+	string destination_city;
+	int cost;
+};
+class CountryGraph
+{
+	unordered_map<string, list<edge>>cities;
+public:
+	void AddCity(string city);
+	void AddEdge(string city_1, string city_2, int cost);
+	bool FindCity(string city);
+	bool FindEdge(string city_1, string city_2);
+	void DisplayGraph();
+	void DisplayCities();
+	void DisplayEdges();
+	void DeleteCity(string city);
+	void DeleteEdge(string city_1, string city_2);
+	void Delete_AllGraph();
+	int Write_Cities_InFiles();
+	int Write_Edges_InFiles();
+	int Read_Cities_FromFiles();
+	int Read_Edges_FromFiles();
+};
